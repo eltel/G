8a61c1c665d09f918b91c88ba42d94ad65daf135
Sysoso <OssielTR@gmail.com>
2017-01-16 09:13:15 +0100

    G-2
    G react-create app version

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+// import methodStubs from './client/configs/method_stubs';
+// import actions from './client/modules/core/actions';
+// // import routes from './routes.jsx';
+//
+// export default {
+//   // routes,
+//   actions,
+//   load(context) {
+//     methodStubs(context);
+//   }
+// };
+
+
+
+ReactDOM.render(
+<App />,
+  document.getElementById('root')
+);
---
+++ src/server/publications/galaxies.js
@@ -0,0 +1,9 @@
+// import Meteor from 'meteor/meteor';
+// import Galaxies from '/lib/schemas/galaxies';
+//
+// import smartPublish from "meteor/utilities:smart-publications";
+//
+//
+// export default function () {
+//   Galaxies.smartPublish("galaxies.list")
+// };
