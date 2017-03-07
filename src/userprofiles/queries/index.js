74f482d35e3aeadc75860a6f1c3777f4772e8ff8
Nayelo <hm@getgee.xyz>
2017-03-07 15:49:42 -0300

    Set queries for User Profiles

--- src/userprofiles/queries/index.js
+++ src/userprofiles/queries/index.js
@@ -1,8 +1,41 @@
-// export default {
-//     FETCH_USER_CONSTELLATIONS_REQUEST: function (action) {
+export default {
+    FETCH_USERS_REQUEST: function (action) {
 
-//     },
-//     CREATE_CONSTELLATIONS_REQUEST: function (action) {
+    },
+    USER_LOGIN_REQUEST: function (action) {
 
-//     },
-// };
+    },
+    USER_LOGOUT_REQUEST: function (action) {
+
+    },
+    CREATE_USER_REQUEST: function (action) {
+
+    },
+    EDIT_USER_REQUEST: function (action) {
+
+    },
+    DELETE_USER_REQUEST: function (action) {
+
+    },
+    ADD_TRUST_REQUEST: function (action) {
+
+    },
+    REVOKE_TRUST_REQUEST: function (action) {
+
+    },
+    ADD_WATCH_REQUEST: function (action) {
+
+    },
+    REVOKE_WATCH_REQUEST: function (action) {
+
+    },
+    ADD_SUPPORT_REQUEST: function (action) {
+
+    },
+    REVOKE_SUPPORT_REQUEST: function (action) {
+
+    },
+    SET_TRUST_DEGREES_REQUEST: function (action) {
+
+    },
+};
