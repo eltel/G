9b7f341a4ce872721fbdac0064d44147a10919d4
Nayelo <hm@getgee.xyz>
2017-03-07 16:59:01 -0300

    Add Galaxy queries

--- src/galaxies/queries/index.js
+++ src/galaxies/queries/index.js
@@ -1,8 +1,26 @@
 export default {
-    FETCH_USER_CONSTELLATIONS_REQUEST: function (action) {
+    FETCH_USER_GALAXIES_REQUEST: function (action) {
 
     },
-    CREATE_CONSTELLATIONS_REQUEST: function (action) {
+    CREATE_GALAXY_REQUEST: function (action) {
+
+    },
+    EDIT_GALAXY_REQUEST: function (action) {
+
+    },
+    DELETE_GALAXY_REQUEST: function (action) {
+
+    },
+    LIST_CONSTELLATIONS_REQUEST: function (action) {
+
+    },
+    ADD_CONSTELLATIONS_REQUEST: function (action) {
+
+    },
+    DELETE_CONSTELLATIONS_REQUEST: function (action) {
+
+    },
+    EDIT_FILTER_REQUEST: function (action) {
 
     },
 };
