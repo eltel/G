55261c1eee50a9eb49fe8c46dd73830dc24e3543
Nayelo <hm@getgee.xyz>
2017-03-07 13:36:49 -0300

    Add metaverse actions

--- src/actions.js
+++ src/actions.js
@@ -2,10 +2,12 @@ import * as core from './core/actions/core-redux';
 import * as constellations from './constellations/actions';
 import * as galaxies from './galaxies/actions';
 import * as universe from './universe/actions';
+import * as metaverse from './metaverse/actions';
 
 export default {
     ...core,
     ...constellations,
     ...galaxies,
     ...universe,
+    ...metaverse,
 };
