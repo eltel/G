04bc34fbcfaf465b462804a45c61caa69dc96281
Nayelo <hm@getgee.xyz>
2017-03-27 16:32:31 -0300

    Remove Create  titles

--- src/core/components/title.jsx
+++ src/core/components/title.jsx
@@ -1,18 +1,15 @@
 import React, { Component } from 'react';
 
 var headings = {
-//   'searchuniverse': 'Search for people, events, organizations, things or ideas.',
-//   'searchconstellation': 'Search for the name of a constellation.',
-//   'searchgalaxy': 'Search for the name of a galaxy.',
-//   'createuniverse': 'Create a person, event, organization, thing or idea.',
-//   'createconstellation': 'Create a constellation of people, events, organizations, things and ideas.',
-//   'creategalaxy': 'Create a galaxy of constellations.'
+	'searchuniverse': 'Search for people, events, organizations, things or ideas.',
+	'searchconstellation': 'Search for the name of a constellation.',
+	'searchgalaxy': 'Search for the name of a galaxy.'
 };
 
 class Title extends React.Component {
   render() {
-    const {origin, direction} = this.props;
-//     const heading = headings[origin+direction];
+    const {direction} = this.props;
+    const heading = headings[direction];
     return (
       <div>
         <h2>
