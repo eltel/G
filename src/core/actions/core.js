864164c50326c28c34d65603f0e6e5516202a138
Nayelo <hm@getgee.xyz>
2017-03-27 16:28:05 -0300

    Remove Origin handling

--- src/core/actions/core.js
+++ src/core/actions/core.js
@@ -1,7 +1,4 @@
 export default {
-  setOrigin( state, origin ) {
-    return state.set('ORIGIN', origin);
-  },
   setDirection(state, direction) {
     return state.set('DIRECTION', direction);
   }
