a0abda6aea7d362477ce81ec4bde799a5a4dfc98
Nayelo <hm@getgee.xyz>
2017-03-28 13:48:15 -0300

    Added future description of landing page for clarity

--- src/core/components/SearchBar.js
+++ src/core/components/SearchBar.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
 import Title from '../components/title.jsx';
 
+// Landing page will eventually contain top constellations of the day or something similar. 
+// At that time, the Search button will bring down a diagonal slide translucent search modal 
+//    that looks like the one in the initial spec (including the advanced search option at the 
+//    bottom)
+// Feel free to recreate this search bar in that manner and link it from the search button in navigation
+
 class SearchBar extends Component {
   constructor(props) {
     super(props);
